2899fc20daf37dc0124d959cffd79705232e3b2d
Francesco Verdoja <francesco.verdoja@aalto.fi>
2022-02-08 11:27:51 +0200

    implemented KTH dataset evaluation

---
+++ deepflow/evaluation.py
@@ -0,0 +1,83 @@
+import numpy as np
+import torch
+from PIL import Image
+
+from deepflow.utils import Direction, OccupancyMap, Window
+from deepflow.nets import DiscreteDirectional
+
+
+def track2pixels(track: np.ndarray, occupancy: OccupancyMap) -> np.ndarray:
+    r = occupancy.resolution
+    o = occupancy.origin
+    s = occupancy.map.size
+    pixels = np.empty((2, track.shape[1]))
+    pixels[0, :] = (track[0, :] - o[0]) / r
+    pixels[1, :] = s[1] - 1 - (track[1, :] - o[1]) / r
+    return pixels
+
+
+def pixels2grid(
+    pixels: np.ndarray, grid_resolution: float, occupancy: OccupancyMap
+) -> np.ndarray:
+    r = occupancy.resolution
+    ratio = r / grid_resolution
+    cell_indeces = (pixels * ratio).astype(int)
+    grid = []
+    last_cell = np.array([np.nan, np.nan])
+    pixels_in_cell = []
+    for i, cell in enumerate(cell_indeces.T):
+        if all(cell == last_cell):
+            pixels_in_cell.append((pixels.T)[i])
+        else:
+            if len(pixels_in_cell):
+                grid.append(
+                    np.concatenate(
+                        (np.mean(pixels_in_cell, axis=0), last_cell)
+                    )
+                )
+            pixels_in_cell = [(pixels.T)[i]]
+            last_cell = cell
+    if len(pixels_in_cell):
+        grid.append(
+            np.concatenate((np.mean(pixels_in_cell, axis=0), last_cell))
+        )
+    return np.array(grid).T
+
+
+def track_likelihood(
+    track: np.ndarray,
+    occupancy: OccupancyMap,
+    window_size: int,
+    scale: int,
+    net: DiscreteDirectional,
+    device: torch.device,
+) -> float:
+    window = Window(window_size * scale)
+    net.to(device)
+    net.eval()
+    like = 0
+    for i in range(track.shape[1] - 1):
+        x, y = track[0:2, i]
+        next_x, next_y = track[0:2, i + 1]
+        center = (int(y), int(x))
+        dir = Direction.from_points((x, y), (next_x, next_y))
+        crop = (
+            np.asarray(
+                occupancy.binary_map.crop(window.corners(center)).resize(
+                    (window_size, window_size), Image.ANTIALIAS
+                ),
+                "float",
+            )
+            / 255.0
+        )
+        inputs = torch.from_numpy(np.expand_dims(crop, axis=(0, 1))).to(
+            device, dtype=torch.float
+        )
+        with torch.no_grad():
+            pred = (
+                net(inputs)[0, :, window_size // 2, window_size // 2]
+                .cpu()
+                .numpy()
+            )
+        like += pred[dir] / (track.shape[1] - 1)
+    return like
---
+++ evaluation_kth.py
@@ -0,0 +1,133 @@
+# %% Some magic
+# ! %load_ext autoreload
+# ! %autoreload 2
+# %% Imports
+
+import random
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import torch
+from PIL import Image
+
+from deepflow.nets import DiscreteDirectional
+from deepflow.evaluation import track2pixels, pixels2grid, track_likelihood
+from deepflow.utils import Window, estimate_dynamics, plot_quivers
+from mod.OccupancyMap import OccupancyMap
+
+BASE_PATH = Path("/mnt/hdd/datasets/KTH_track/")
+MAP_METADATA = BASE_PATH / "map.yaml"
+MAP_PGM = BASE_PATH / "map.pgm"
+TRACKS_DATA = BASE_PATH / "dataTrajectoryNoIDCell6251.mat"
+
+EPOCHS = 100
+WINDOW_SIZE = 64
+SCALE = 8
+# GRID_SCALE = SCALE
+GRID_SCALE = 20
+
+DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+PLOT_DPI = 800
+
+occupancy = OccupancyMap.from_yaml(MAP_METADATA)
+tracks: np.ndarray = sio.loadmat(TRACKS_DATA, squeeze_me=True)[
+    "dataTrajectoryNoIDCell"
+]
+
+id_string = f"_w{WINDOW_SIZE}_s{SCALE}_t_{EPOCHS}"
+
+net = DiscreteDirectional(WINDOW_SIZE)
+window = Window(WINDOW_SIZE * SCALE)
+
+path = f"./people_net{id_string}.pth"
+checkpoint = torch.load(path)["model_state_dict"]
+net.load_state_dict(checkpoint)
+
+
+def show_occupancy(occupancy: OccupancyMap) -> None:
+    r = occupancy.resolution
+    o = occupancy.origin
+    sz = occupancy.map.size
+    extent = (o[0], o[0] + sz[0] * r, o[1], o[1] + sz[1] * r)
+    plt.imshow(occupancy.map, extent=extent, cmap="gray")
+
+
+# %%
+
+plt.figure(dpi=PLOT_DPI)
+show_occupancy(occupancy)
+
+# ids = range(len(tracks))
+# ids = [5101]
+ids = [random.randint(0, len(tracks) - 1) for i in range(10)]
+
+for id in ids:
+    t: np.ndarray = tracks[id]
+
+    X = t[0, :]
+    Y = t[1, :]
+
+    plt.plot(X, Y, linewidth=0.5)
+    # plt.scatter(X, Y, s=0.05)
+
+# %%
+
+plt.figure(dpi=PLOT_DPI)
+plt.imshow(
+    occupancy.map,
+    cmap="gray",
+)
+for id in ids:
+    p = track2pixels(tracks[id], occupancy)
+    t = pixels2grid(p, occupancy.resolution * GRID_SCALE, occupancy)
+    X = t[0, :]
+    Y = t[1, :]
+    U = t[2 + 0, :] * GRID_SCALE + GRID_SCALE / 2
+    V = t[2 + 1, :] * GRID_SCALE + GRID_SCALE / 2
+    plt.plot(p[0, :], p[1, :], "x-", markersize=0.1, linewidth=0.1)
+    plt.plot(X, Y, "o", markersize=0.1, linewidth=0.1)
+    plt.grid(True, linewidth=0.1)
+    plt.xticks(range(0, occupancy.map.size[0], GRID_SCALE))
+    plt.yticks(range(0, occupancy.map.size[1], GRID_SCALE))
+    plt.scatter(U, V, s=0.1)
+
+# %%
+
+center = (691, 925)
+inputs = (
+    np.asarray(
+        occupancy.binary_map.crop(window.corners(center)).resize(
+            (WINDOW_SIZE, WINDOW_SIZE), Image.ANTIALIAS
+        ),
+        "float",
+    )
+    / 255.0
+)
+
+outputs = estimate_dynamics(net, inputs, device=DEVICE, batch_size=32)
+
+plot_quivers(inputs, outputs, dpi=PLOT_DPI)
+plt.plot(WINDOW_SIZE // 2, WINDOW_SIZE // 2, "o", markersize=0.5)
+
+# %%
+
+evaluation_ids = range(len(tracks))
+# evaluation_ids = [5101]
+# evaluation_ids = [random.randint(0, len(tracks) - 1) for i in range(10)]
+
+like = 0.0
+skipped = 0
+for id in evaluation_ids:
+    p = track2pixels(tracks[id], occupancy)
+    t = pixels2grid(p, occupancy.resolution * GRID_SCALE, occupancy)
+    if t.shape[1] > 1:
+        like += track_likelihood(t, occupancy, WINDOW_SIZE, SCALE, net, DEVICE)
+    else:
+        skipped += 1
+print(
+    f"chance: {1 / 8}, total like: {like:.3f}, avg like: "
+    f"{like / (len(evaluation_ids)-skipped):.3f} "
+    f"(on {(len(evaluation_ids)-skipped)} tracks, {skipped} skipped)"
+)
